fdbae473fd1aa2eaf7b7348155cf2c7bb841504b
PANGJINI <geniejini97@gmail.com>
2023-12-03 23:48:13 +0900

    first commit

---
+++ src/main/java/com/MdTrading/MdTradingApplication.java
@@ -0,0 +1,13 @@
+package com.MdTrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MdTrading/MdTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MdTrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
